7fa2956c70b88ed32b34735ade673ef4c0cbd451
Georgi Kirilov <gkkirilov@gmail.com>
2024-09-16 21:28:39 +0300

    Update site

--- pages/jobs/index.vue
+++ pages/jobs/index.vue
@@ -202,16 +202,7 @@
               </PopoverGroup>
             </div>
           </section>
-  
-          <!-- Job listings -->
-          <section aria-labelledby="products-heading" class="mt-8">
-            <h2 id="products-heading" class="sr-only">Обяви за работа</h2>
-  
-            <div class="space-y-6">
-              <JobListItem v-for="job in jobs" :key="job.id" :job="job" />
-            </div>
-          </section>
-  
+
           <section aria-labelledby="featured-heading" class="relative mt-16 overflow-hidden rounded-lg lg:h-96">
             <div class="absolute inset-0">
               <img src="https://tailwindui.com/img/ecommerce-images/category-page-01-featured-collection.jpg" alt="" class="h-full w-full object-cover object-center" />
@@ -226,6 +217,16 @@
               <a href="#" class="mt-6 flex flex-shrink-0 items-center justify-center rounded-md border border-white border-opacity-25 bg-white bg-opacity-0 px-4 py-3 text-base font-medium text-white hover:bg-opacity-10 sm:ml-8 sm:mt-0 lg:ml-0 lg:w-full">Вижте топ работодателите</a>
             </div>
           </section>
+  
+          <!-- Job listings -->
+          <section aria-labelledby="products-heading" class="mt-8">
+            <h2 id="products-heading" class="sr-only">Обяви за работа</h2>
+  
+            <div class="space-y-6">
+              <JobListItem v-for="job in jobs" :key="job.id" :job="job" />
+            </div>
+          </section>
+
         </div>
       </main>
 
